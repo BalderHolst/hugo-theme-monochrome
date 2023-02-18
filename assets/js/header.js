f429700a3709250aad2b4823b91ee3d4b6a6ba5c
Balder W. Holst <balderwh@gmail.com>
2023-02-18 13:34:22 +0100

    fixup! Light mode default

--- assets/js/header.js
+++ assets/js/header.js
@@ -34,10 +34,14 @@ window.addEventListener("DOMContentLoaded", function () {
     const dark_mode_btn = document.getElementById("dark_mode_btn");
     const light_mode_btn = document.getElementById("light_mode_btn");
 
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    if (localStorage.theme === 'dark' || !('theme' in localStorage)) {
+        console.log("dark");
         dark_mode_btn.classList.remove('hidden');
+        document.documentElement.setAttribute("data-theme", "light");
     } else {
+        console.log("light");
         light_mode_btn.classList.remove('hidden');
+        document.documentElement.setAttribute("data-theme", "light");
     }
 
     dark_mode_btn.addEventListener('click', function () {
